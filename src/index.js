707ce48e4092fdc13d15049816a4e9a81108744a
Pedro Gomez <makhnnar@gmail.com>
2020-03-24 10:48:01 -0400

    changed function version for class version

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
-import React from "react"
+import React, {Component} from 'react'
 
 import Highlight, { defaultProps } from "prism-react-renderer";
 
-const PrismComponent = ({codeToShow,langSintax}) => {
-    return (
-        <Highlight 
-            {...defaultProps} 
-            code={codeToShow} 
-            language={langSintax}>
-            {({ className, style, tokens, getLineProps, getTokenProps }) => (
-            <pre className={className} style={style}>
-                {tokens.map((line, i) => (
-                <div {...getLineProps({ line, key: i })}>
-                    {line.map((token, key) => (
-                    <span {...getTokenProps({ token, key })} />
-                    ))}
-                </div>
-                ))}
-            </pre>
-            )}
-        </Highlight>
-    )
-}
 
-export default PrismComponent;
+export default class PrismComponent extends Component{
+
+    render() {
+        const {
+            codeToShow,
+            langSintax
+        } = this.props
+        return( <Highlight 
+                {...defaultProps} 
+                code={codeToShow} 
+                language={langSintax}>
+                {({ className, style, tokens, getLineProps, getTokenProps }) => (
+                <pre className={className} style={style}>
+                    {tokens.map((line, i) => (
+                    <div {...getLineProps({ line, key: i })}>
+                        {line.map((token, key) => (
+                        <span {...getTokenProps({ token, key })} />
+                        ))}
+                    </div>
+                    ))}
+                </pre>
+                )}
+        </Highlight>);
+      }
+
+}
